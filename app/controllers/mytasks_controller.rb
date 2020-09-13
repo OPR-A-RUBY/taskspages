10b24e4201e46602d6de8b449de130351d4313a9
OPR-A-RUBin <opr.a.ruby@gmail.com>
2020-09-13 23:54:33 +0400

    Add @mytasks for views

--- app/controllers/mytasks_controller.rb
+++ app/controllers/mytasks_controller.rb
@@ -1,7 +1,17 @@
 class MytasksController < ApplicationController
 
 	def index
-		
+		@mytasks = Mytask.all
 	end
+
+	def show
+  end
+	
+	def new
+	  @mytasks = Mytask.new
+  end
+
+  def edit
+  end
 	
 end
